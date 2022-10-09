9d076cdcf3838fa80249879f3b3f24ab06f92f9d
mgh18 <m.ghazi.asad.168@gmail.com>
2022-10-09 14:59:21 +0330

    first commit

---
+++ simple web app/Pages/Index.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace simple_web_app.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+        public string message { get; set; }
+        public void OnGet()
+        {
+            message = "hello from model";
+            ViewData["message"] = "hello from view data";
+            ViewData["number"] = 100;
+
+        }
+        [BindProperty]
+        public int Length { get; set; }
+        [BindProperty]
+        public int Width { get; set; }
+        public IActionResult OnPost()
+        {
+            var area = Length * Width;
+            TempData["area"] = area;
+            return RedirectToPage("./Result");
+        }
+    }
+}
---
+++ simple web app/Pages/Result.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace simple_web_app.Pages
+{
+    public class ResultModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
